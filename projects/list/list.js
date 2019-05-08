54474f17021e57b050aa2d13a6ba56ef9f07ea02
Evan Finnigan <evanfinnigan@gmail.com>
2019-05-07 21:42:24 -0400

    Adding functionality: add custom image, remove all

--- projects/list/list.js
+++ projects/list/list.js
@@ -1,3 +1,5 @@
+var idNum = 0;
+
 function allowDrop(ev) {
 	ev.preventDefault();
 }
@@ -9,11 +11,43 @@ function drag(ev) {
 function drop(ev) {
 	ev.preventDefault();
 	var data = ev.dataTransfer.getData("text");
+	
 	if (ev.target.classList.contains("drag_box"))
 	{
 		ev.target.parentElement.appendChild(document.getElementById(data));
 	}
 	else {
 		ev.target.appendChild(document.getElementById(data));
+	}
+}
+
+function addItem() {
+	var txt = document.getElementById("insert").value;
+	document.getElementById("insert").value = "";
+	
+	// Check img URL
+	if (txt.match(/\.(jpeg|jpg|gif|png)$/) == null)
+	{
+		return;
+	}
+
+	//newItem.innerHTML = txt;
+	var img = document.createElement("img");
+	img.setAttribute("id", ("newItem" + window.idNum));
+	idNum++;
+	img.setAttribute("class", "drag_box");
+	img.setAttribute("draggable", "true");
+	img.setAttribute("ondragstart", "drag(event)");
+	img.src = txt;
+	
+	document.getElementById("unrank_box").appendChild(img);
+}
+
+function clearItems() {
+	var unrank_box = document.getElementById("unrank_box");
+	
+	while (unrank_box.firstChild)
+	{
+		unrank_box.removeChild(unrank_box.firstChild);
 	}
 }
